87bddc356a05661eeae41df80dd128c38e751aba
Bowie <lfranckx@thinkful.com>
2023-08-03 19:54:05 -0500

    fix offset

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ export default function Header() {
                         <div className={active ? 'logo-wrap active' : 'logo-wrap'}>
                             <Link 
                                 onClick={() => setActive(!active)}
-                                activeClass="active" to="solutions" spy={true} smooth={true} offset={0} duration={500}
+                                activeClass="active" to="solutions" spy={true} smooth={true} offset={-80} duration={500}
                             >
                                 Solutions
                             </Link>
@@ -82,7 +82,7 @@ export default function Header() {
                     </div>
                     <h1><span>(</span> Quintessential Unboxing Experience <span>)</span></h1>
                     <div className='btn-wrap'>
-                        <Link className='btn-sec' activeClass="active" to="solutions" spy={true} smooth={true} offset={120} duration={500}>Learn how we’re revolutionizing shipping</Link>
+                        <Link className='btn-sec' activeClass="active" to="solutions" spy={true} smooth={true} offset={-120} duration={500}>Learn how we’re revolutionizing shipping</Link>
                     </div>
                 </div>
             </motion.header>
